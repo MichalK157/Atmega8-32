7be5776689ff77263a0b84662dbd1bc3d83cb0ca
MichalK157 <mkac@borg-reman.local>
2018-10-30 09:44:23 +0100

    first

---
+++ CS_AMT8/Init.c
@@ -0,0 +1,95 @@
+/*
+ * Init.c
+ *
+ * Created: 30.10.2018 06:09:59
+ *  Author: mkac
+ */ 
+
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#define F_CPU 8000000UL
+#include <util/delay.h>
+#include <stdlib.h>
+
+volatile static unsigned char h,l;
+
+void SendChar(char data)
+{
+	while(!(UCSRA&(1<<UDRE)));
+	{
+		UDR=data;
+	}
+}
+
+void UartInit(void)
+{
+	DDRD|=(1<<PIND1);
+	DDRD&=~(1<<PIND0);
+	PORTD|=1<<(PIND0);
+	
+	#define BAUD 9600
+	#include <util/setbaud.h>
+	
+	UBRRH=UBRRH_VALUE;
+	UBRRL=UBRRL_VALUE;
+	
+	UCSRC = (1<<URSEL) | (1<<UCSZ1) | (1<<UCSZ0);
+	UCSRB = (1<<TXEN) | (1<<RXEN);
+	_delay_ms(10);
+	SendChar(0x00);
+}
+
+void SendADCValue(unsigned char _chaneladdress,uint16_t data)
+{
+	h=(unsigned char)(data>>8);
+	l=(unsigned char)(data);
+	SendChar(_chaneladdress);
+	SendChar(h);
+	SendChar(l);
+}
+
+void PWMInit(void)
+{
+	DDRB|=(1<<PINB1)|(1<<PINB2);
+	//TCCR1A |= (1<<WGM12)|(1<<WGM10);		//8bit Fast PWM
+	//TCCR1B |= (1<<WGM12)|(1<<WGM10);
+	TCCR1A |= (1<<WGM12)|(1<<WGM11)|(1<<WGM10);	//10bit Fast PWM
+	TCCR1B |= (1<<WGM12)|(1<<WGM11)|(1<<WGM10);
+	TCCR1A |= (1<<COM1A1)|(1<<COM1B1);	//
+	TCCR1B |= (1<<CS10);	//Prescaler 1 |(1<<CS11)
+	TCCR1A |= (1<<CS10);
+}
+
+void PWM(uint16_t _pwm)
+{
+	OCR1A=_pwm;
+	OCR1B=_pwm;
+}
+
+void ADCInit()
+{
+	ADMUX|=(1<<REFS0);
+	ADCSRA|=((1<<ADEN)|(1<<ADIE)|(1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0));
+	_delay_ms(500);
+
+}
+
+uint16_t ReadAdc(uint8_t chanel)
+{
+	chanel&=0x07;
+	ADMUX=(ADMUX&0xf8)|chanel;
+	ADCSRA|=(1<<ADSC);
+	while(ADCSRA&(1<<ADSC));
+	return ADC;
+}
+
+void OnAdc()
+{
+	ADCSRA|=(1<<ADEN);
+}
+
+void OffAdc()
+{
+	ADCSRA&=~(1<ADEN);
+}
---
+++ CS_AMT8/main.c
@@ -0,0 +1,91 @@
+/*
+ * CS_AMT8.c
+ *
+ * Created: 29.10.2018 10:36:18
+ * Author : mkac
+ */ 
+
+#include <avr/io.h>
+#define F_CPU 8000000UL
+#include <util/delay.h>
+#include <avr/interrupt.h>
+#include "Init.h"
+
+volatile static	uint8_t _Status= 1;
+volatile static uint16_t adc_board;
+volatile static uint16_t adc_sensor1;
+volatile static uint16_t adc_sensor2;
+volatile static uint16_t adc_sensor3;
+volatile const uint8_t __ch_board=6;	//chanel0
+volatile const uint8_t __ch_sensor1=0;	//chanel1
+volatile const uint8_t __ch_sensor2=1;	//chanel2
+volatile const uint8_t __ch_sensor3=2;	//chanel3
+volatile const uint8_t _add_ch_board=0x20;	//chanel0
+volatile const uint8_t _add_ch_sensor1=0x21;	//chanel1
+volatile const uint8_t _add_ch_sensor2=0x22;	//chanel2
+volatile const uint8_t _add_ch_sensor3=0x23;	//chanel3
+
+int main(void)
+{
+	DDRD|=(1<<PIND7);	//EN12
+	DDRB|=(1<<PINB0);	//EN34
+	PORTD|=(1<<PIND7);
+	PORTB|=(1<<PINB0);
+	DDRD &=~((1<<PIND2)|(1<<PIND3)); //input interr
+	
+	ADCInit();
+	PWMInit();
+	UartInit();
+	
+	PWM(0);
+	
+	GICR=(1<<INT0);
+	MCUCR|=(1<<ISC00);		//		(1<<ISC00);any logical change			//	(1<<ISC01)|(1<<ISC00);rising edge
+	
+	sei();
+	
+    while (1) 
+    {		
+		OnAdc();
+		adc_board=ReadAdc(__ch_board);
+		_delay_ms(5);
+		adc_sensor1=ReadAdc(__ch_sensor1);
+		_delay_ms(5);
+		if (_Status)
+		{	
+			//Uart comunication Send data do pc
+			SendADCValue(_add_ch_board,adc_board);
+			_delay_ms(5);
+			SendADCValue(_add_ch_sensor1,adc_sensor1);
+			_delay_ms(5);
+		}
+		OffAdc();
+		_delay_ms(1000);
+	}
+}
+
+ISR(ADC_vect)
+{
+	if(adc_sensor1<150){
+		PWM(300);
+	}
+	else {
+		if(adc_sensor1*2>1023){
+			PWM(1023);
+		}
+		else{
+			PWM(adc_sensor1*2);
+		}
+	}
+}
+ISR(INT0_vect)
+{
+	if(_Status==1)
+	{
+		_Status=0;
+	}
+	else
+	{
+		_Status=1;
+	}
+}
